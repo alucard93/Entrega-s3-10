91cf1fab92f78db5487b150fee1ece2805681ea2
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2022-04-12 19:47:38 -0300

    Initial Commit

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ console.log(positions(2,1,0))    // Definidos os valores de cada atleta é só c
 //console.log(positions(1,2,0)) 
 //console.log(positions(0,1,2)) 
 
-
 //Assumidos os valores de Daniel = [2] Obs: posição 2, definida na linha 3 do VSC.
 //Assumidos os valores de Manoel = [1] Obs: posição 1, definida na linha 3 do VSC. 
 //Assumidos os valores de Rafael = [0] Obs: posição 0, definida na linha 3 do VSC. 
